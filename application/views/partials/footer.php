72ac5baf5050b86b933d90104d65c338cc340a58
Daffa Tris <daffats4@gmail.com>
2025-07-11 12:51:51 +0700

    Add files via upload

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © Daffa Tris | 2025</a></span>
+		</div>
+	</div>
+</footer>
